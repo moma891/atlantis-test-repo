25321c0dd0e83439edbe33bbcf5ce7191efe879a
Momcilo Dejanovic <momcilo@globaldatanet.com>
2022-09-26 01:27:26 +0200

    atlantis test

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+
+  required_version = "~> 1.0.4"
+}
+
+provider "aws" {
+  region = var.aws_region
+
+}
+
+provider "random" {}
+
+#State design
+terraform {
+  backend "s3" {
+    dynamodb_table = "terraform-lock"
+    key            = "infra/terraform.tfstate"
+    region         = "eu-central-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 2.3.0"
+    }
+  }
+}
